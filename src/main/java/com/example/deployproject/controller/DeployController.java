06e99ae41684a6e0ddf0013a38702a36ab57fb3c
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-09-05 10:34:53 -0300

    Se agrega nuevo endpoint

--- src/main/java/com/example/deployproject/controller/DeployController.java
+++ src/main/java/com/example/deployproject/controller/DeployController.java
@@ -4,9 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("deploy")
 public class DeployController {
+
+    Map<String, String> users = new HashMap<>();
+
+    @GetMapping
+    public Map<String, String> getAllUsers(){
+        buildUsers();
+        return users;
+    }
+
+    private void buildUsers() {
+        users.put("Ali", "Hey!");
+        users.put("Haider", "How are you!");
+    }
 
     @GetMapping("hello")
     public String helloWorld(){
